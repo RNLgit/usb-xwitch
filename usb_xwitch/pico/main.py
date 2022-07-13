bde33baf82feb4e3085b5b7678f62f6fd03013c6
RunnanLi <lirunnan90@gmail.com>
2022-07-13 21:41:12 +1000

    intr added

--- usb_xwitch/pico/main.py
+++ usb_xwitch/pico/main.py
@@ -13,19 +13,37 @@ ADC_1_2 = 27  # CHA mus 2-2 VBus voltage
 ADC_RATIO = 18/33  # ADC voltage divider ratio
 ADC_REF_V = 3.3  # ADC reference voltage
 
-led_pico = Pin(PICO_LED, Pin.OUT)
-adc_a1 = ADC(ADC_1_1)
-adc_a2 = ADC(ADC_1_2)
-
-print('usb-xwitch v0.1\nby RNL\n')
+led_pico_stat = False
 
 
 def pico_led(en: bool) -> None:
-    led_pico.value(en)
+    """
+    toggle on off of pico onboard led
+    """
+    _led_pico.value(en)
+
+
+def flip_pico_led():
+    """
+    flip pico onboard led status
+    """
+    global led_pico_stat
+    led_pico_stat = not led_pico_stat
+    pico_led(led_pico_stat)
 
 
 def get_adc(no: int) -> float:
-    adc = {1: adc_a1, 2: adc_a2}
+    """
+    get adc reading from adc channel
+    """
+    adc = {1: _adc_a1, 2: _adc_a2}
     if adc.get(no) is None:
         raise ValueError(f'adc {no} invalid')
     return adc.get(no).read_u16() * ADC_REF_V / 65536 / ADC_RATIO
+
+
+_led_pico = Pin(PICO_LED, Pin.OUT)
+_adc_a1 = ADC(ADC_1_1)
+_adc_a2 = ADC(ADC_1_2)
+_swa = Pin(SW_A, Pin.IN, Pin.PULL_DOWN)
+_swa.irq(trigger=Pin.IRQ_RISING, handler=flip_pico_led)
